deafbd945ce9448885eb1b2d56e00f6f47620da8
Dariusz.Żulpo <dd2000@wp.pl>
2019-05-18 15:20:05 +0200

    singleton 20190518

--- src/main/java/pl/sdacademy/designpatterns/decorator/DecoratorDemo.java
+++ src/main/java/pl/sdacademy/designpatterns/decorator/DecoratorDemo.java
@@ -3,5 +3,18 @@ package pl.sdacademy.designpatterns.decorator;
 public class DecoratorDemo {
     public static void main(String[] args) {
 
+// bez dekoratora
+//        final EngineStarter engineStarter = new Car();
+//
+//        engineStarter.startEngine();
+//        engineStarter.stopEngine();
+
+        // wykorzystanie obu dekoratorów
+        final EngineStarter engineStarter = new CarEngineStatusAfterLoggingDecorator(
+                new CarEngineStatusBeforeLoggingDecorator(new Car()));
+
+        engineStarter.startEngine();
+        engineStarter.stopEngine();
+
     } // main()
 }
